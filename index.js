bd142e4151fde5a3bc85cc590a4e00f59e26cee1
StefanGorgevik <stefangorgevik@hotmail.com>
2020-02-06 21:53:09 +0100

    c2
    niza objekti response send

--- index.js
+++ index.js
@@ -13,6 +13,67 @@ server.get('/', function (request, response) {
     response.status(200).send('You have reached server!')
 })
 
+// So query parametar
+server.get('/users', (request, response) => {
+
+    try {
+
+        const array = ['Petko', 'Stanko', 'Laki']
+
+        // The route param value is in the request object
+        
+        const name = request.query.name
+        //includes vrakja samo true ili false ( dali postoi vo objektot )
+        if (name && array.includes(name)) {
+            response.status(200).send(name)
+        }
+            else {
+            response.status(200).send(array)
+            }
+    }
+    catch (error) {
+        console.error(error)
+        response.status(500).send('An error occured')
+    }
+})
+
+//Search users by id
+server.get('/users/:id', (request, response) => {
+
+    const array = ['Petko', 'Stanko', 'Laki']
+
+    // The route param value is in the request object
+    
+    const id = request.params.id
+    //Faulty value (ako ne e tocno)
+    if (array[id]) {
+        response.status(200).send(array[id])
+    }
+    else {
+        response.status(200).send('No data found')
+    }
+})
+
+
+
+//Search Users by Name
+/* server.get('/users/:name', (request, response) => {
+    const array = ['Petko', 'Stanko', 'Laki']
+
+    const name = request.params.name.toLowerCase()
+
+    const found = array.find((element) => element.toLowerCase() === name)
+
+    if (found) {
+        response.status(200).send(found)
+    }
+    else {
+        response.status(200).send('No data found')
+    }
+}) */
+
+
+
 // server.post('/')
 // server.delete('/')
 
